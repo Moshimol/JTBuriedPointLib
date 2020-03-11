51145dca9d3e19f571cae8d5cefb426fb399bb7f
lushitong <lushitong@qq.com>
2020-03-11 16:23:58 +0800

    初始化项目
    Signed-off-by: lushitong <lushitong@qq.com>

---
+++ Example/Pods/Target Support Files/JTBuriedPointLib/JTBuriedPointLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JTBuriedPointLib : NSObject
+@end
+@implementation PodsDummy_JTBuriedPointLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-JTBuriedPointLib_Example/Pods-JTBuriedPointLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JTBuriedPointLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JTBuriedPointLib_Example
+@end
