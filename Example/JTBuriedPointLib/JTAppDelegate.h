1f63e8fef5084ff1ed16eec524d82b9535e8d4c9
lushitong <lushitong@qq.com>
2020-03-11 16:22:13 +0800

    Initial commit

---
+++ Example/JTBuriedPointLib/JTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JTAppDelegate.h
+//  JTBuriedPointLib
+//
+//  Created by lushitong on 03/11/2020.
+//  Copyright (c) 2020 lushitong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface JTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/JTBuriedPointLib/JTViewController.h
@@ -0,0 +1,13 @@
+//
+//  JTViewController.h
+//  JTBuriedPointLib
+//
+//  Created by lushitong on 03/11/2020.
+//  Copyright (c) 2020 lushitong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface JTViewController : UIViewController
+
+@end
